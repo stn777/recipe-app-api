b6e7063d704e5b7e51ff2c817e484fc52a7f674d
stn777 <iserv@i.ua>
2019-03-24 21:28:13 +0200

    Added changes

--- app/core/models.py
+++ app/core/models.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
     PermissionsMixin
 from django.conf import settings
 
+
 def recipe_image_file_path(instance, filename):
     """Generate file path for new recipe image"""
     ext = filename.split('.')[-1]
